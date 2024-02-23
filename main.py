1220f98647bdb2cc3eb2f3e227fc956c5a2232b0
Peter McCabe <104118252+pjmcc2@users.noreply.github.com>
2024-02-22 19:56:47 -0800

    addind tmu

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from tqdm import tqdm
 from tmu.models.classification import vanilla_classifier
 import os
 import json
-from pyTsetlinMachine.tm import MultiClassTsetlinMachine
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
 
@@ -41,19 +40,21 @@ if __name__ == "__main__":
     #integer_weighted = ["weights"]
     #drop_clause = ["drop"]
     # Many more optional parameters
-    #model = vanilla_classifier.TMClassifier(number_clauses, 
-    #                                        T=T,
-    #                                        s=s,
-    #                                        number_of_state_bits_ta=state_bits,
-    #                                        incremental=True)
+    model = vanilla_classifier.TMClassifier(number_clauses, 
+                                            T=T,
+                                            s=s,
+                                            number_of_state_bits_ta=state_bits,
+                                            incremental=True)
   
-    epochs = config["epochs"]
-    model = MultiClassTsetlinMachine(number_clauses,T,s,number_of_state_bits=state_bits)
+    #epochs = config["epochs"]
+    epochs = 2
     #train loop
     train_accuracy_list = []
     for e in tqdm(range(epochs)):
         model.fit(train_x,train_y,epochs=1,incremental=True)
         train_preds = model.predict(train_x)
+        print(f"predictions of shape: {train_preds.shape}, first element: {train_preds[0]}")
+        
         train_acc = np.mean(train_preds == train_y)
         train_accuracy_list.append(train_acc)
 
