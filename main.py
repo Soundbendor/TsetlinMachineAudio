2e40ecc093a40a8b026f0f3319ac0ced01b1bc0c
Peter McCabe <104118252+pjmcc2@users.noreply.github.com>
2024-02-22 19:51:30 -0800

    skeleton train loop

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import torch
 import numpy as np
 from tqdm import tqdm
+from tmu.models.classification import vanilla_classifier
+import os
+import json
 from pyTsetlinMachine.tm import MultiClassTsetlinMachine
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+
 
 #neptune starts here
 
@@ -15,25 +20,51 @@ if __name__ == "__main__":
 
     #TODO add pickling of train/test acc per epoch, and final preds for use in f1/precision/recall scores, charts, etc.
     # Also log the trian config files
-    
+    current_directory = os.getcwd()
+    with open("config_npy.json", 'r') as f:
+        config = json.load(f)
 
     # Data stuff
-    train_x = np.load("/nfs/guille/eecs_research/soundbendor/mccabepe/VocalSet/train_X.npy")
-    train_y = np.load("/nfs/guille/eecs_research/soundbendor/mccabepe/VocalSet/train_y.npy")
+    
+    train_x = np.load(config["train_x"])
+    train_y = np.load(config["train_y"])
+    assert len(train_y.shape) == 1
 
     #val_x = np.load("/nfs/guille/eecs_research/soundbendor/mccabepe/VocalSet/val_X.npy")
     #val_y = np.load("/nfs/guille/eecs_research/soundbendor/mccabepe/VocalSet/val_y.npy")
  
-    num_classes = 5
-    number_clauses = 240*num_classes
-    s = 6
-    T = 17
+    num_classes = config["num_classes"]
+    number_clauses = config["clauses"]
+    s = config["s"]
+    T = config["T"]
+    state_bits = config["state_bits"]
+    #integer_weighted = ["weights"]
+    #drop_clause = ["drop"]
+    # Many more optional parameters
+    #model = vanilla_classifier.TMClassifier(number_clauses, 
+    #                                        T=T,
+    #                                        s=s,
+    #                                        number_of_state_bits_ta=state_bits,
+    #                                        incremental=True)
+  
+    epochs = config["epochs"]
+    model = MultiClassTsetlinMachine(number_clauses,T,s,number_of_state_bits=state_bits)
+    #train loop
+    train_accuracy_list = []
+    for e in tqdm(range(epochs):)
+        model.fit(train_x,train_y,epochs=1,incremental=True)
+        train_preds = model.predict(train_x)
+        train_acc = np.mean(train_preds == train_y)
+        train_accuracy_list.append(train_acc)
+
+    plt.plot(np.arange(epochs),train_accuracy_list)
+    plt.title("Train Accuracy")
+    plt.xlabel("Epochs")
+    plt.ylabel("Accuracy")
+
+    conf_m = confusion_matrix(train_y,train_preds)
+    print(conf_m)
 
 
-
-    model = MultiClassTsetlinMachine(number_clauses, T=T,s=s)
-
-    
-    epochs = 150
   
 
