5b61c50feb258349a47695f4b3132011fc4278c1
Peter McCabe <104118252+pjmcc2@users.noreply.github.com>
2024-03-13 11:56:24 -0700

    cpu version

--- job_submit.sh
+++ job_submit.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #
 #SBATCH --job-name=TMAudio_test
-#SBATCH --partition=dgxs,dgx2,dgxh
-#SBATCH --gres=gpu:1
-#SBATCH --constraint=el9
+#SBATCH --partition=soundbendor
+#SBATCH --account=soundbendor
+#SBATCH --cpus-per-task = 4
 #SBATCH -o sbatch_logs/main.out
 #SBATCH -e sbatch_logs/main.err
 
@@ -14,3 +14,13 @@ module load python/3.10
 module load openssl/1.1.1w
 source env/bin/activate
 python3 tmu_test.py
+
+
+### OLD
+
+#SBATCH --job-name=TMAudio_test
+#SBATCH --partition=dgxs,dgx2,dgxh
+#SBATCH --gres=gpu:1
+#SBATCH --constraint=el9
+#SBATCH -o sbatch_logs/main.out
+#SBATCH -e sbatch_logs/main.err
