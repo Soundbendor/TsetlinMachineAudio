7893733ce271977b6ca0907aa6010122a80edd41
Peter McCabe <104118252+pjmcc2@users.noreply.github.com>
2024-03-13 10:49:05 -0700

    test run

--- job_submit.sh
+++ job_submit.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #
-#SBATCH --job-name=TMAudio
-#SBATCH --partition=dgxh
-#SBATCH --gres=gpu:2
+#SBATCH --job-name=TMAudio_test
+#SBATCH --partition=dgxs,dgx2
+#SBATCH --gres=gpu:1
 #SBATCH --constraint=el9
 #SBATCH -o sbatch_logs/main.out
 #SBATCH -e sbatch_logs/main.err
@@ -11,4 +11,4 @@ module load slurm
 module load cuda/12.2
 module load gcc/11.4
 source env/bin/activate
-python3 main.py
+python3 tmu_test.py
