1cb5bd6da19e069e42115597016d5211ae7cae1e
Peter McCabe <104118252+pjmcc2@users.noreply.github.com>
2024-03-15 16:12:19 -0700

    removed tqdm

--- tmu_test.py
+++ tmu_test.py
@@ -1,17 +1,16 @@
 import numpy as np
 from tmu.data import MNIST
 from tmu.models.classification.vanilla_classifier import TMClassifier
-from tqdm import tqdm
 import pycuda
 
 
 if __name__ == "__main__":
   
     data = MNIST().get()
-    tm  =TMClassifier(1000,40,5,incremental=True,platform="GPU")
+    tm = TMClassifier(1000,40,5,incremental=True,platform="GPU")
 
     batch_size = 300
-    for i in tqdm(range(int(60000/300))):
+    for i in range(int(60000/300)):
         x_batch = data["x_train"][i*batch_size:(i+1)*batch_size]
         y_batch = data['y_train'][i*batch_size:(i+1)*batch_size]
         tm.fit(x_batch,y_batch,epochs=1)
