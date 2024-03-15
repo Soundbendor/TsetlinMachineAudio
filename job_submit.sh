d71371213a890f78bd79d401bf302b7ae27b50c1
Peter McCabe <104118252+pjmcc2@users.noreply.github.com>
2024-03-15 15:21:16 -0700

    test

--- job_submit.sh
+++ job_submit.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #
-#SBATCH --job-name=TMAudio
+#SBATCH --job-name=TM_test
 #SBATCH --partition=soundbendor
 #SBATCH --account=soundbendor
-#SBATCH --cpus-per-task=16
-#SBATCH --mem=10G
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=1G
 #SBATCH -o sbatch_logs/main.out
 #SBATCH -e sbatch_logs/main.err
 
@@ -14,10 +14,36 @@ module load gcc/12.2
 module load python/3.10
 module load openssl/1.1.1w
 source env/bin/activate
-python3 main.py
+module list
+which gcc
+gcc --version
+which nvcc
+nvcc --version
+which python3
+python3 --version
+python3 tmu_test.py
 
 
-### OLD
+### CURRENT
+#SBATCH --job-name=TMAudio
+#SBATCH --partition=soundbendor
+#SBATCH --account=soundbendor
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=10G
+#SBATCH -o sbatch_logs/main.out
+#SBATCH -e sbatch_logs/main.err
+
+#module load slurm
+#module load cuda/12.2
+#module load gcc/12.2
+#module load python/3.10
+#module load openssl/1.1.1w
+#source env/bin/activate
+#ython3 main.py
+
+
+
+### OLDER
 
 #SBATCH --job-name=TMAudio_test
 #SBATCH --partition=dgxs,dgx2,dgxh
