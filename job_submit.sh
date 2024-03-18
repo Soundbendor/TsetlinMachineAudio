8cb3c6e85b4afd80ba2d84f666171a8b311f42ad
Peter McCabe <104118252+pjmcc2@users.noreply.github.com>
2024-03-17 17:13:30 -0700

    big run

--- job_submit.sh
+++ job_submit.sh
@@ -13,7 +13,7 @@ module load cuda/12.2
 module load gcc/11.4
 module load python/3.10
 source env/bin/activate
-python3 main.py 2500 5 200 False 1 config_main.json
+python3 main.py 2500 5 200 False 100 config_main.json
 
 # ARGS: clauses, s, T, weights, epochs, config_file
 # Vowel Set up: 2500, 5, 200, False, 100, config_main.json
