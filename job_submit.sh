0d36a0a89bc3047450d956d090c943c55d1b02ca
Peter McCabe <104118252+pjmcc2@users.noreply.github.com>
2024-03-19 11:50:26 -0700

    tests

--- job_submit.sh
+++ job_submit.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 #
+#SBATCH --job-name=TMU-TEST
+#SBATCH --partition=dgxs,dgxh,dgx2
+#SBATCH --time=2-00:00:00
+#SBATCH --gres:gpu=1
+#SBATCH -o sbatch_logs/main.out
+#SBATCH -e sbatch_logs/main.err
+
+
+source activate env/bin/activate
+module load cuda/12.2
+python tmu_test.py
+
+
+# ARGS: clauses, s, T, weights, epochs, config_file
+# Vowel Set up: 2500, 5, 200, False, 100, config_main.json
+
+
+
 #SBATCH --job-name=TMAudio
 #SBATCH --partition=soundbendor
 #SBATCH --account=soundbendor
@@ -10,12 +28,12 @@
 #SBATCH -e sbatch_logs/main.err
 
 
-module load slurm
-module load cuda/12.2
-module load gcc/11.4
-module load python/3.10
-source env/bin/activate
-python3 main.py 2500 5 200 False 100 config_main.json
+#module load slurm
+#module load cuda/12.2
+#module load gcc/11.4
+#module load python/3.10
+#source env/bin/activate
+#python3 main.py 2500 5 200 False 100 config_main.json
 
 # ARGS: clauses, s, T, weights, epochs, config_file
 # Vowel Set up: 2500, 5, 200, False, 100, config_main.json
